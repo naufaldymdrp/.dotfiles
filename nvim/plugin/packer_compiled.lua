0b1fc103849473c465fe0cb8e4511971503eeba2
naufaldymdrp <naufaldygood@outlook.com>
2022-01-02 10:39:15 +0700

    Menghapus file packer_compiled.lua
    File itu tidak usah dicommit karena hasil kompilasi
    packer plugin manager
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
     Changes to be committed:
    	modified:   nvim/plugin/packer_compiled.lua
    
     Changes not staged for commit:
    	modified:   nvim/lua/plugins.lua

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/naufal/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/naufal/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/naufal/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/naufal/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/naufal/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "C:\\Users\\NAUFAL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?.lua;C:\\Users\\NAUFAL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\NAUFAL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\NAUFAL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
+local install_cpath_pattern = "C:\\Users\\NAUFAL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\lua\\5.1\\?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,73 +71,73 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["barbar.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/barbar.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\barbar.nvim",
     url = "https://github.com/romgrk/barbar.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   ["cmp-vsnip"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-vsnip",
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   ["colorbuddy.vim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/colorbuddy.vim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\colorbuddy.vim",
     url = "https://github.com/tjdevries/colorbuddy.vim"
   },
   ["gruvbuddy.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/gruvbuddy.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gruvbuddy.nvim",
     url = "https://github.com/tjdevries/gruvbuddy.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-colorizer.lua"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lsp-installer",
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0" },
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
@@ -145,52 +145,52 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = true,
     only_cond = false,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/opt/nvim-treesitter",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-ts-autotag"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-ts-rainbow"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-ts-rainbow",
     url = "https://github.com/p00f/nvim-ts-rainbow"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["toggleterm.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\toggleterm.nvim",
     url = "https://github.com/akinsho/toggleterm.nvim"
   },
   ["trouble.nvim"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\trouble.nvim",
     url = "https://github.com/folke/trouble.nvim"
   },
   ["vim-vsnip"] = {
     loaded = true,
-    path = "/home/naufal/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    path = "C:\\Users\\NaufaldyMDRP_95\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-vsnip",
     url = "https://github.com/hrsh7th/vim-vsnip"
   }
 }
