16e40a7dc10909c07a7a252a8c0c3240e78cbed7
naufaldymdrp <naufaldygood@outlook.com>
2022-10-03 23:47:42 +0700

    Perubahan di nvim-cmp-config

--- nvim/lua/configs/nvim-cmp-config.lua
+++ nvim/lua/configs/nvim-cmp-config.lua
@@ -35,8 +35,8 @@ cmp.setup({
     { name = 'calc'},                               -- source for math calculation
   },
   window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
   },
   formatting = {
       fields = {'menu', 'abbr', 'kind'},
