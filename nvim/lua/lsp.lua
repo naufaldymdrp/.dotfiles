520d0e0c690cf88a4a50058cbbe64ac120a558cd
naufaldymdrp <naufaldygood@outlook.com>
2022-10-07 22:20:14 +0700

    Perbaikan konfig karena Git conflicts

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -12,12 +12,9 @@ lspconfig.omnisharp.setup({})
 lspconfig.tsserver.setup({})
 lspconfig.svelte.setup({})
 lspconfig.astro.setup({})
-<<<<<<< Updated upstream
 lspconfig.pyright.setup({})
-=======
 lspconfig.clangd.setup({})
 lspconfig.cmake.setup({})
->>>>>>> Stashed changes
 
 lspconfig.rust_analyzer.setup({
     settings = {
