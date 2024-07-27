fa7bf04033b4ff5ec997e8912d5c711a1af40d4d
naufaldymdrp <naufaldymdrp@outlook.com>
2024-07-28 06:18:11 +0700

    fix: added code to automatically installs mason lsp
    - also added code to recognize nvim's lua api correctly

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,6 +1,8 @@
 -- ---------------------------------------------------------------------------
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    automatic_installation = true
+})
 
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -22,18 +24,19 @@ lspconfig.denols.setup({
 })
 lspconfig.zls.setup({})
 lspconfig.serve_d.setup({})
-lspconfig.nimls.setup({})
+-- lspconfig.nimls.setup({})
 lspconfig.gleam.setup({})
 lspconfig.ruff_lsp.setup({})
 -- lspconfig.phan.setup({})
 lspconfig.intelephense.setup({})
+-- lspconfig.gopls.setup({})
 
 -- local on_attach = function (client)
 --     require("completion").on_attach(client)
 -- end
 
 lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     settings = {
         ["rust-analyzer"] = {
             imports = {
@@ -54,59 +57,61 @@ lspconfig.rust_analyzer.setup({
     }
 })
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "init.lua")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+-- local runtime_path = vim.split(package.path, ';')
+-- table.insert(runtime_path, "init.lua")
+-- table.insert(runtime_path, "lua/?.lua")
+-- table.insert(runtime_path, "lua/?/?.lua")
+-- table.insert(runtime_path, "lua/?/init.lua")
 
-local library = {}
+-- local library = {}
 
-local function add(lib)
-    for _, p in pairs(vim.fn.expand(lib, false, true)) do
-        p = vim.loop.fs_realpath(p)
-        library[p] = true
-    end
-end
+-- local function add(lib)
+--     for _, p in pairs(vim.fn.expand(lib, false, true)) do
+--         p = vim.loop.fs_realpath(p)
+--         library[p] = true
+--     end
+-- end
 
 -- add runtime libraries
-add("$VIMRUNTIME") -- vim runtime libraries themself
-add("$VIMRUNTIME/lua") -- vim runtime libraries themself
-add("$VIMRUNTIME/lua/vim/lsp") -- vim runtime libraries themself
-add("$XDG_CONFIG_HOME/nvim") -- vim lua plugin script
+-- add("$VIMRUNTIME") -- vim runtime libraries themself
+-- add("$VIMRUNTIME/lua") -- vim runtime libraries themself
+-- add("$VIMRUNTIME/lua/vim/lsp") -- vim runtime libraries themself
+-- add("$XDG_CONFIG_HOME/nvim") -- vim lua plugin script
 -- add vim libraries added by packer
-if vim.fn.has('win32') == 1 then
-    add("$XDG_DATA_HOME/nvim-data/site/pack/packer/opt/*")
-    add("$XDG_DATA_HOME/nvim-data/site/pack/packer/start/*")
-else
-    add("$XDG_DATA_HOME/nvim/site/pack/packer/opt/*")
-    add("$XDG_DATA_HOME/nvim/site/pack/packer/start/*")
-end
+-- if vim.fn.has('win32') == 1 then
+--     add("$XDG_DATA_HOME/nvim-data/site/pack/packer/opt/*")
+--     add("$XDG_DATA_HOME/nvim-data/site/pack/packer/start/*")
+-- else
+--     add("$XDG_DATA_HOME/nvim/site/pack/packer/opt/*")
+--     add("$XDG_DATA_HOME/nvim/site/pack/packer/start/*")
+-- end
 
 lspconfig.teal_ls.setup({})
 lspconfig.lua_ls.setup({
-    settings = {
-            Lua = {
-                runtime = {
-                    -- Lua version
-                    version = 'LuaJIT',
-                    path = runtime_path,
-                },
-                completion = { callSnippet = "Both" },
-                diagnostics = {
-                    globals = {'vim', },
-                },
-                workspace = {
-                    library = library,
-                    maxPreload = 2000,
-                    preloadFileSize = 50000,
-                },
-                -- do not send telemetry data
-                telemetry = {
-                    enable = false,
-                },
-            }
-    }
+  on_init = function(client)
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 })
 
 
