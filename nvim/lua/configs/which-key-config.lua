d238a97cf0c0faf0f28c42485f43d8c6c1c25a60
naufaldymdrp <naufaldymdrp@outlook.com>
2024-07-28 06:22:51 +0700

    fix: changed mapping to use new api spec
    - mappings using nested object still works btw, it's just that i follow suggestion from ':checkhealth which-key' wkwkwk
    - also this registers mappings using add function

--- nvim/lua/configs/which-key-config.lua
+++ nvim/lua/configs/which-key-config.lua
@@ -1,46 +1,38 @@
 local wk = require("which-key")
 local lsp = vim.lsp;
-local diag = vim.diagnostic
 
-local mappings = {
-    f = {
-        name = "Telescope",
-        f = { "<CMD>Telescope find_files<CR>", "Find files in workspace" },
-        b = { "<CMD>Telescope buffers<CR>", "Find opened files in current buffer" },
-        s = { "<CMD>Telescope grep_string<CR>", "Search for a string under cursor" },
-        S = { "<CMD>Telescope live_grep<CR>", "Search for a string live as type" },
-        r = { "<CMD>Telescope lsp_references<CR>", "Lists LSP references for a word under cursor" },
-        d = { "<CMD>Telescope lsp_definitions<CR>", "Lists LSP definitions for a word under cursor" },
-        ['ds'] = { "<CMD>Telescope lsp_document_symbols<CR>", "Lists LSP document symbols in current buffer" },
-        ['ws'] = { "<CMD>Telescope lsp_workspace_symbols<CR>", "Lists LSP document symbols in current workspace" },
-        g = { "<CMD>Telescope git_bcommits<CR>", "Lists git commits in current buffer" },
-        G = { "<CMD>Telescope git_commits<CR>", "Lists git commit in current workspace" },
-    },
-    l = {
-        name = "LSP Controls",
-        k = { function() lsp.buf.hover() end, "Hover" },
-        K = { "<CMD>Lspsaga hover_doc<CR>", "Lspsaga's Hover Documentation" },
-        f = { function() lsp.buf.format({ async = true }) end, "Format current buffer" },
-        d = { "<CMD>Lspsaga show_cursor_diagnostics<CR>", "LSPSaga : Show Cursor Diagnostics" },
-        D = { "<CMD>Lspsaga show_line_diagnostics<CR>", "LSPSaga : Show Line Diagnostics" },
-        ['gd'] = { function() lsp.buf.definition() end, "Go to Definitions" },
-        ['gD'] = { function() lsp.buf.declaration() end, "Go to Declarations" },
-    },
-    h = { "<cmd>nohls<cr>", "No highlights" },
-    t = {
-        name = "Trouble plugin",
-        t = { "<CMD>TroubleToggle document_diagnostics<CR>", "Trouble Info for Document Diagnostics" },
-        T = { "<CMD>TroubleToggle workspace_diagnostics<CR>", "Trouble Info for Workspace Diagnostics" },
-        r = { "<CMD>TroubleToggle lsp_references<CR>", "Trouble Info for LSP References" },
-        d = { "<CMD>TroubleToggle lsp_definitions<CR>", "Trouble Info for LSP Definitions" },
-    },
-    g = {
-        name = "Gitsigns Controls",
-        b = { "<CMD>Gitsigns blame_line<CR>", "Show current line git blame"},
-    },
-}
-local options = {
-    prefix = "<leader>"
+local mappings =
+{
+    { "<leader>f",   group = "Telescope" },
+    { "<leader>fG",  "<CMD>Telescope git_commits<CR>",               desc = "Lists git commit in current workspace" },
+    { "<leader>fS",  "<CMD>Telescope live_grep<CR>",                 desc = "Search for a string live as type" },
+    { "<leader>fb",  "<CMD>Telescope buffers<CR>",                   desc = "Find opened files in current buffer" },
+    { "<leader>fd",  "<CMD>Telescope lsp_definitions<CR>",           desc = "Lists LSP definitions for a word under cursor" },
+    { "<leader>fds", "<CMD>Telescope lsp_document_symbols<CR>",      desc = "Lists LSP document symbols in current buffer" },
+    { "<leader>ff",  "<CMD>Telescope find_files<CR>",                desc = "Find files in workspace" },
+    { "<leader>fg",  "<CMD>Telescope git_bcommits<CR>",              desc = "Lists git commits in current buffer" },
+    { "<leader>fr",  "<CMD>Telescope lsp_references<CR>",            desc = "Lists LSP references for a word under cursor" },
+    { "<leader>fs",  "<CMD>Telescope grep_string<CR>",               desc = "Search for a string under cursor" },
+    { "<leader>fws", "<CMD>Telescope lsp_workspace_symbols<CR>",     desc = "Lists LSP document symbols in current workspace" },
+    -- ------------------------------------------------------------------------
+    { "<leader>g",   group = "Gitsigns Controls" },
+    { "<leader>gb",  "<CMD>Gitsigns blame_line<CR>",                 desc = "Show current line git blame" },
+    { "<leader>h",   "<cmd>nohls<cr>",                               desc = "No highlights" },
+    -- ------------------------------------------------------------------------
+    { "<leader>l",   group = "LSP Controls" },
+    { "<leader>lD",  "<CMD>Lspsaga show_line_diagnostics<CR>",       desc = "LSPSaga : Show Line Diagnostics" },
+    { "<leader>lK",  "<CMD>Lspsaga hover_doc<CR>",                   desc = "Lspsaga's Hover Documentation" },
+    { "<leader>ld",  "<CMD>Lspsaga show_cursor_diagnostics<CR>",     desc = "LSPSaga : Show Cursor Diagnostics" },
+    { "<leader>lf",  function() lsp.buf.format() end,                desc = "Format current buffer" },
+    { "<leader>lgD", function() lsp.buf.declaration() end,           desc = "Go to Declarations" },
+    { "<leader>lgd", function() lsp.buf.definition() end,            desc = "Go to Definitions" },
+    { "<leader>lk",  function() lsp.buf.hover() end,                 desc = "Hover" },
+    -- ------------------------------------------------------------------------
+    { "<leader>t",   group = "Trouble plugin" },
+    { "<leader>tT",  "<CMD>TroubleToggle workspace_diagnostics<CR>", desc = "Trouble Info for Workspace Diagnostics" },
+    { "<leader>td",  "<CMD>TroubleToggle lsp_definitions<CR>",       desc = "Trouble Info for LSP Definitions" },
+    { "<leader>tr",  "<CMD>TroubleToggle lsp_references<CR>",        desc = "Trouble Info for LSP References" },
+    { "<leader>tt",  "<CMD>TroubleToggle document_diagnostics<CR>",  desc = "Trouble Info for Document Diagnostics" },
 }
 
-wk.register(mappings, options)
+wk.add(mappings)
